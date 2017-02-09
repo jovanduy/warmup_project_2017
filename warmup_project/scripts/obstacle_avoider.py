0420aa088720867ec0f4d72692a117390bbd4923
kimasenbeck <kimasenbeck@users.noreply.github.com>
2017-02-09 18:07:18 -0500

    Added docstrings and name==main

--- warmup_project/scripts/obstacle_avoider.py
+++ warmup_project/scripts/obstacle_avoider.py
@@ -11,6 +11,7 @@ import mathimport sys
 
 class ObstacleNode(object):
     def __init__(self):
+        """This function initializes the rospy node, our publishers and subscribers, and initializes relevant variables."""
         rospy.init_node('obstacle_avoider')
         self.r = rospy.Rate(5)
         self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
@@ -24,16 +25,16 @@ class ObstacleNode(object):
         
         
     def stop(self):
+        """This function stops the neato."""
         self.publisher.publish(Twist(linear=Vector3(0.0, 0.0, 0.0), angular=Vector3(0.0, 0.0, 0.0)))
         
-    
     def process_scan(self, m):
+        """This function is the callback for our laser subscriber. In this function, we detect whether there is an obstacle in front of the neato or next to the neato"""
         self.ranges = m.ranges
         if (self.ranges[0] < 1 and self.ranges[0] != 0):            
             self.obstacle_front = True
         else:
             self.obstacle_front = False
-
         print self.ranges[270]
         if (self.ranges[270] < 1 and self.ranges[270] != 0):
             self.obstacle_side = True
@@ -46,8 +47,10 @@ class ObstacleNode(object):
             return self.go_along
         self.twist= Twist(linear=Vector3(0.0, 0.0, 0.0), angular=Vector3(0.0, 0.0, 0.5))
         self.publisher.publish(self.twist)
-        return self.turn_left        
+        return self.turn_left  
+    
     def turn_right(self):
+        """This function publishes a twist that makes the neato turn right. The turn is timed, and we turn just long enough to complete a 90 degree turn"""
         if (rospy.Time.now() - self.time >= rospy.Duration(3)):
             self.time = rospy.Time.now()            
             return self.move_towards_goal
@@ -56,9 +59,12 @@ class ObstacleNode(object):
         return self.turn_right
         
     def go_forward(self):
+        """This function publishes a twist that makes the neato move forward"""
         self.twist = Twist(linear=Vector3(1.0, 0.0, 0.0), angular=Vector3(0.0, 0.0, 0.0))
         self.publisher.publish(self.twist)
+        
     def go_along(self):
+        """This function publishes a twist that makes the neato move forward until it has cleared the obstacle at its side."""
         if (rospy.Time.now() - self.time >= rospy.Duration(1)):
             if not self.obstacle_side:
                 self.time = rospy.Time.now()
@@ -71,24 +77,24 @@ class ObstacleNode(object):
             return self.go_along
 
     def move_towards_goal(self):
+        """This function sends the neato forward until it encounters an obstacle, in which case it changes states to the left turn state"""
         if self.obstacle_front:
             self.time = rospy.Time.now()
             return self.turn_left
         else:
             self.go_forward()
-            return self.move_towards_goal
-        
+            return self.move_towards_goal      
         
     def run(self):
+        """This function sets the current state to move_towards_goal to begin with, and then updates that state whenever a new state is returned in a state change."""
         rospy.on_shutdown(self.stop)
         curr_state = self.move_towards_goal
         while not rospy.is_shutdown():           
             
             curr_state = curr_state()
-            print curr_state
               
             self.r.sleep()
         
-            
-obstacle_node = ObstacleNode()
-obstacle_node.run()
+if __name__ == "__main__":            
+    obstacle_node = ObstacleNode()
+    obstacle_node.run()
